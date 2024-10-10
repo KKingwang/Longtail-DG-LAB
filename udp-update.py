4ad31ab2e39ead52c447e53194a18d3088bd4ea3
空白 <53444337+KKingwang@users.noreply.github.com>
2024-10-10 13:21:01 +0800

    更新，规范文本

--- udp-update.py
+++ udp-update.py
@@ -33,8 +33,9 @@ def create_wave_sequence(mode, sequence_a, sequence_b):
 
 if __name__ == '__main__':
     # 示例使用
-    modes = "on_a"  # 可以是任意长度的字符串
+    modes = "on_a"  # 输入模式（on_a, on_b, on_all）
 
+    # 频谱以列表形式写出，上线分别为31，1023，31，具体信息看 v2 文档自行研究，列表禁止为 None，否则会报错，可以为（0，0，0）。
     wave_sequences_a = [
         (5, 135, 20), (5, 125, 20), (5, 115, 20), (5, 105, 20),
         (5, 95, 20), (4, 86, 20), (4, 76, 20), (4, 66, 20),
